886d8bdb3a77713b545af801fa28f547998fb261
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-05-13 11:30:19 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Routes>
+        <Route path={`/`} element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
