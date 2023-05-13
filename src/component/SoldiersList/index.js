2158d6066a0fb0e538a5a9b9a8e3a645b829a1fa
Vlad Foroshcha <vladfo2003@gmail.com>
2023-05-14 01:17:48 +0300

    feat: adapt soldiers list

---
+++ src/component/SoldiersList/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
+import SoldiersCards from './SoldiersCards';
+import { SoldiersTable } from './SoldiersTable';
+
+function createData(id, name, age, bloodType, infections, alergions, battleRoster) {
+  return { id, name, age, bloodType, infections, alergions, battleRoster };
+}
+
+const rows = [
+  createData(1, 'Вале́рій Фе́дорович Залу́жний', 25, 'R(II) -', 'none', 'none', 'No Info', 'Alpha Team'),
+  createData(2, 'Вале́рій Фе́дорович Залу́жний', 5, 'R(II) -', 'none', 'none', 'No Info', 'Beta Team'),
+  createData(3, 'Вале́рій Фе́дорович Залу́жний', 2, 'R(II) -', 'none', 'none', 'No Info', 'Gamma Team'),
+];
+
+const SoldiersList = () => {
+  const isMobile = useWindowSize();
+
+  return isMobile ? <SoldiersCards data={rows} /> : <SoldiersTable rows={rows} />;
+};
+
+export default SoldiersList;
--- src/constants/common.js
+++ src/constants/common.js
@@ -1,7 +1,15 @@
 export const TABLE_MIN_WIDTH = 650;
-export const FEEDBACK_TABLE_HEADERS = {
-  NAME: 'ПІБ',
-  HELP_DATE: 'Дата надання допомоги',
-  FEEDBACK: 'Відгук',
-};
+
+export const FEEDBACK_TABLE_HEADERS = ['ПІБ', 'Дата надання допомоги', 'Відгук'];
 export const FEEDBACK_DATA_ORDER = ['name', 'helpDate', 'feedback'];
+
+export const SOLDIERS_TABLE_HEADERS = [
+  'ПІБ',
+  'Бригада',
+  'Вік',
+  'Група крові',
+  'Інфекції',
+  'Алергії',
+  'Переливання крові',
+];
+export const SOLDIERS_DATA_ORDER = ['name', 'id', 'age', 'bloodType', 'infections', 'alergions', 'battleRoster'];
